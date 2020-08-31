570122e1521b173d4ad3db86464c5117fb08ed64
ooo A ooo <jacobsanner@Jacobs-MacBook-Air.local>
2020-08-31 12:51:22 -0400

    I swear to fucking god

--- longo.js
+++ longo.js
@@ -2,7 +2,7 @@ const longo = require('mongoose');
 const requireDir = require('require-dir');
 const dir = requireDir('./schema');
 
-longo.connect('mongodb+srv://tweetums:6ItDBa3tqfMFfL2Q@broumvirate-com-wvuvq.mongodb.net/tweetums?retryWrites=true&w=majority', {
+longo.connect(LONGO_TOKEN, {
     useNewUrlParser: true,
     useUnifiedTopology: true 
 });
