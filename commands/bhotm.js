4eda36f90ce33e41840dc2c75e5323f158a8d377
Ben Hagle <ben.hagle@gmail.com>
2020-09-16 11:24:50 -0400

    Replaced depricated momentjs with dayjs for BHotM date processing

--- commands/bhotm.js
+++ commands/bhotm.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
-const moment = require("moment");
+const dayjs = require("dayjs");
 
 module.exports.help = "**&bhotm:** Reminds you when BHotM is due.";
 
 module.exports.command = async (message) => {
-    let distance = dueMoment().diff(moment());
+    let distance = dueMoment().diff(dayjs());
 
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
@@ -31,17 +31,17 @@ function dueMoment(incJ) {
     //Returns next due date moment
     //Params: incJ:boolean. include judging period. If true, due date doesn't roll over until the 8th. if false or empty, rolls over on 5th.
     let day = 4;
-    dueDate = moment();
+    let dueDate;
 
     if (incJ) {
         day = 7;
     }
-    if (moment().get("date") > day) {
+    if (dayjs().date() > day) {
         //if it's after the 4th, add a month and set submission time
-        dueDate.add(1, "M").date(5).startOf("day");
+        dueDate = dayjs().add(1, "M").date(5).startOf("day");
     } else {
         //if it's before the 4th, its this month
-        dueDate.date(5).startOf("day");
+        dueDate = dayjs().date(5).startOf("day");
     }
     return dueDate;
 }
