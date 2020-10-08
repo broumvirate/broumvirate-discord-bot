c2ffca52c0ffb4f6da965c8e3acdbab14b66a07a
bhagle <ben.hagle@gmail.com>
2020-10-07 22:44:21 -0400

    Added then/catch to twitter handler

--- twitterhandler.js
+++ twitterhandler.js
@@ -1,21 +1,27 @@
-require("dotenv").config()
-const Discord = require("discord.js")
-const Twitter = require("twitter-lite")
-const fs = require("fs")
+require("dotenv").config();
+const Discord = require("discord.js");
+const Twitter = require("twitter-lite");
+const fs = require("fs");
 
 const twitclient = new Twitter({
     subdomain: "api",
     consumer_key: process.env.APP_KEY, // from Twitter.
     consumer_secret: process.env.APP_SECRET, // from Twitter.
     access_token_key: process.env.ACC_KEY, // from your User (oauth_token)
-    access_token_secret: process.env.ACC_SECRET // from your User (oauth_token_secret)
+    access_token_secret: process.env.ACC_SECRET, // from your User (oauth_token_secret)
 });
 
 module.exports.tweet = (message, tweetcontent) => {
-
-    twitclient.post("statuses/update", {
-        status: tweetcontent.content
-    });
-    
-    message.channel.send(`Successfully tweeted '${tweetcontent.content}' to the Broumvirate account.`);
-}
+    twitclient
+        .post("statuses/update", {
+            status: tweetcontent.content,
+        })
+        .then(() => {
+            message.channel.send(
+                `Successfully tweeted '${tweetcontent.content}' to the Broumvirate account.`
+            );
+        })
+        .catch(() => {
+            message.channel.send("Failed to tweet to the Broumvirate account.");
+        });
+};
