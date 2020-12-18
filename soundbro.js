201eadeda4ccf26f96458384903445b06bc21549
bhagle <ben.hagle@gmail.com>
2020-12-18 11:03:32 -0500

    Added soundbro list

--- soundbro.js
+++ soundbro.js
@@ -2,13 +2,23 @@ const fs = require("fs");
 
 module.exports = function (client) {
     let leaveTimer;
+    const activityOptions = [
+        "with himself",
+        "&4 repeatedly",
+        "Lost in the Woods",
+        "slinkycar",
+        "Just sittin' there",
+    ];
 
     // Set status on bot ready
     client.on("ready", () => {
         console.log("SoundBro ready...");
-        client.user.setActivity("Just sittin' there", {
-            type: "PLAYING",
-        });
+        client.user.setActivity(
+            activityOptions[Math.floor(Math.random * activityOptions.length)],
+            {
+                type: "PLAYING",
+            }
+        );
     });
 
     client.on("message", (message) => {
@@ -16,6 +26,7 @@ module.exports = function (client) {
             // Determine requested file
             let fileNum = Array.from(message.content);
             fileNum.shift();
+
             fileNum = fileNum.join("");
             if (!fileNum) {
                 message.reply(
@@ -24,30 +35,49 @@ module.exports = function (client) {
                 return;
             }
 
-            // Attempt to load file
-            fs.access(`sounds/${fileNum}.mp3`, (err) => {
-                if (err) {
-                    message.reply(
-                        `fuck off. ${fileNum} is not a valid sound file.`
-                    );
-                } else {
-                    // Join voice channel, play sound file
-                    if (message.member.voice.channel) {
-                        message.member.voice.channel
-                            .join()
-                            .then((connection) => {
-                                connection.play(`sounds/${fileNum}.mp3`);
+            if (fileNum == "list") {
+                getList(message);
+            } else {
+                // Attempt to load file
+                fs.access(`sounds/${fileNum}.mp3`, (err) => {
+                    if (err) {
+                        message.reply(
+                            `fuck off. ${fileNum} is not a valid sound file.`
+                        );
+                    } else {
+                        // Join voice channel, play sound file
+                        if (message.member.voice.channel) {
+                            message.member.voice.channel
+                                .join()
+                                .then((connection) => {
+                                    connection.play(`sounds/${fileNum}.mp3`);
 
-                                // Start five minute timer to leave the channel
-                                clearTimeout(leaveTimer);
-                                leaveTimer = setTimeout(() => {
-                                    connection.disconnect();
-                                }, 300000);
-                            })
-                            .catch((err) => console.log(err));
+                                    // Start five minute timer to leave the channel
+                                    clearTimeout(leaveTimer);
+                                    leaveTimer = setTimeout(() => {
+                                        connection.disconnect();
+                                    }, 300000);
+                                })
+                                .catch((err) => console.log(err));
+                        }
                     }
-                }
-            });
+                });
+            }
         }
     });
 };
+
+function getList(message) {
+    fs.readdir("sounds", (err, files) => {
+        if (err) {
+            message.reply("I can't do that.");
+        } else {
+            const sounds = files
+                .filter((el) => el.split(".")[1] == "mp3")
+                .map((el) => el.split(".")[0])
+                .sort((a, b) => a - b);
+            message.channel.send("Listing of all SoundBro options:");
+            message.channel.send(sounds.join(", "));
+        }
+    });
+}
