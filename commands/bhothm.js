e2d78e702cfb6a2b88f62f6abcf11613d6d6eaf7
bhagle <ben.hagle@gmail.com>
2021-02-05 14:41:30 -0500

    Can add bhothmtext

--- commands/bhothm.js
+++ commands/bhothm.js
@@ -3,14 +3,27 @@ const axios = require("axios");
 
 module.exports.help = "**&bhothm:** Generates a random meme.";
 
-module.exports.command = (message) => {
-    axios
-        .get("http://broumvirate.com/api/bhothm")
-        .then((response) => {
-            message.channel.send(" ", { files: [response.data.url] });
-        })
-        .catch((error) => {
-            console.log(error);
-            // message.channel.send("Unable to get meme.");
-        });
+module.exports.command = (message, args) => {
+    if(args.length === 0)
+    {
+        axios
+            .get("http://broumvirate.com/api/bhothm")
+            .then((response) => {
+                message.channel.send(" ", { files: [response.data.url] });
+            })
+            .catch((error) => {
+                console.log(error);
+                message.channel.send("Unable to get meme.");
+            });
+    }
+    else if (args[0] === "add")
+    {
+        const bhothmText = args.slice(1).join(" ");
+        axios.post("http://broumvirate.com/api/bhothm", {text: bhothmText})
+            .then((res) => {
+                message.channel.send(`Added ${bhothmText} to meme database.`)
+            }).catch((error) => {
+                message.channel.send("Unable to add BHOTHMText")
+            })
+    }
 };
