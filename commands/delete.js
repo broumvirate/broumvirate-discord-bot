d3a9c6f0870c6c2f7c8d1771b8203818b53fb92e
bhagle <ben.hagle@gmail.com>
2022-07-20 13:21:44 -0500

    Fix deleting tweet 0

--- commands/delete.js
+++ commands/delete.js
@@ -3,7 +3,7 @@ const longoman = require("../helpers.js");
 const voter = require("../tweetumsVoter");
 
 module.exports.command = async (message, args) => {
-    if(parseInt(args[0]) && longoman.isAdmin(message))
+    if((parseInt(args[0]) || args[0] == "0") && longoman.isAdmin(message))
     {
         if(parseInt(args[0]) < 0 ) return;
         const deleted = await tweetindex.deleteOne({
